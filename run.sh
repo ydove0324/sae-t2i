7bf0894a4c4329bb64651d976e226e8c56625399
ydove0324 <1352035939@qq.com>
2026-01-08 00:03:26 +0800

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-export https_proxy=http://bj-rd-proxy.byted.org:3128
-export http_proxy=http://bj-rd-proxy.byted.org:3128
-export WANDB_BASE_URL=https://api.bandw.top
-export WANDB_KEY="3ff2050be63ca796f027523bc233bc3e70c7668b"
-export ENTITY="fobow"
-export PROJECT="sae-imagenet-noise-1536-e2e-104"
+# export https_proxy=http://bj-rd-proxy.byted.org:3128
+# export http_proxy=http://bj-rd-proxy.byted.org:3128
+# export WANDB_BASE_URL=https://api.bandw.top
+# export WANDB_KEY="3ff2050be63ca796f027523bc233bc3e70c7668b"
+# export ENTITY="fobow"
+# export PROJECT="sae-imagenet-noise-1536-e2e-104"
 wandb offline
 
 MASTER_ADDR=$ARNOLD_WORKER_0_HOST
@@ -22,7 +22,7 @@ echo "  MASTER_PORT=$MASTER_PORT"
 torchrun  --nnodes=$NNODES --node-rank=$NODE_RANK  --nproc-per-node=$NPROC_PER_NODE \
   --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT \
   projects/rae/train.py \
-  --config /opt/tiger/vfm/projects/rae/configs/stage2/training/ImageNet256/DiTDH-XL_DINOv3_1536.yaml \
+  --config /share/project/huangxu/SAE/projects/rae/configs/stage2/training/ImageNet256/DiTDH-XL_DINOv3_1536.yaml \
   --data-path /opt/tiger/dataset/ILSVRC2012_img_train \
   --results-dir ./trash \
   --precision fp32 \
